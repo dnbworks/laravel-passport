f781c12f68bdb26a775db3060e8daaa612e8087b
dnbworks <dooyongnsaakobalderama@gmail.com>
2022-04-19 01:57:41 +0800

    CRUD operation with author table

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->prefix('v1')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
-    Route::get('authors/{author}', [AuthorsController::class, 'show']);
+    
+    Route::apiResource('/authors',  AuthorsController::class);
+ 
 });
